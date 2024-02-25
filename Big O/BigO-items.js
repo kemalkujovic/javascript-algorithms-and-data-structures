db88c907328b07176908f482709ac840a28c5414
Kemal Kujovic <kemalkujovic1111@gmail.com>
2024-02-25 13:42:18 +0100

    first commit

---
+++ Big O/BigO-items.js
@@ -0,0 +1,15 @@
+function logItems(n) {
+  for (let i = 0; i < n; i++) {
+    console.log(i);
+  }
+  for (let j = 0; j < n; j++) {
+    console.log(j);
+  }
+}
+logItems(10);
+
+function addItems(n) {
+  return n + n;
+}
+
+// https://www.bigocheatsheet.com/
