e58e88608d26d65d4311b3f79b7ff6d20da7158d
Kemal Kujovic <kemalkujovic1111@gmail.com>
2024-04-19 21:47:47 +0200

    dsa: Recursion & factorial

---
+++ Recursion/script.js
@@ -0,0 +1,20 @@
+function funcThree() {
+  console.log("Three");
+}
+function funcTwo() {
+  funcThree();
+  console.log("Two");
+}
+function funcOne() {
+  funcTwo();
+  console.log("One");
+}
+
+funcOne();
+
+function factorial(n) {
+  if (n === 1) return 1;
+  return n * factorial(n - 1);
+}
+
+factorial(4);
