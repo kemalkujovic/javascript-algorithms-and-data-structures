821cba0ff6ddd45b257881bb808bcac383716209
Kemal Kujovic <kemalkujovic1111@gmail.com>
2024-04-24 19:47:39 +0200

    DSA: Recursive Binary Search Trees

--- Trees/script.js
+++ Trees/script.js
@@ -58,6 +58,69 @@ class BST {
     }
     return currentNode;
   }
+
+  rContains(value, currentNode = this.root) {
+    if (currentNode === null) return false;
+
+    if (value === currentNode.value) return true;
+
+    if (value < currentNode.value) {
+      return this.rContains(value, currentNode.left);
+    } else {
+      return this.rContains(value, currentNode.right);
+    }
+  }
+
+  #rInsert(value, currentNode = this.root) {
+    if (currentNode === null) return new Node(value);
+
+    if (value < currentNode.value) {
+      currentNode.left = this.#rInsert(value, currentNode.left);
+    } else if (value > currentNode.value) {
+      currentNode.right = this.#rInsert(value, currentNode.right);
+    }
+    return currentNode;
+  }
+
+  rInsert(value) {
+    if (this.root === null) this.root = new Node(value);
+    this.#rInsert(value);
+  }
+
+  #deleteNode(value, currentNode) {
+    if (currentNode === null) return null;
+
+    if (value < currentNode.value) {
+      currentNode.left = this.#deleteNode(value, currentNode.left);
+    } else if (value > currentNode.value) {
+      currentNode.right = this.#deleteNode(value, currentNode.right);
+    } else {
+      if (currentNode.left === null && currentNode.right === null) {
+        return null;
+      } else if (currentNode.left === null) {
+        currentNode = currentNode.right;
+      } else if (currentNode.right === null) {
+        currentNode = currentNode.left;
+      } else {
+        let subTreeMin = this.rminValue(currentNode.right);
+        currentNode.value = subTreeMin;
+        currentNode.right = this.#deleteNode(subTreeMin, currentNode.right);
+      }
+    }
+
+    return currentNode;
+  }
+
+  rminValue(currentNode) {
+    while (currentNode.left != null) {
+      currentNode = currentNode.left;
+    }
+    return currentNode.value;
+  }
+
+  deleteNode(value) {
+    this.root = this.#deleteNode(value, this.root);
+  }
 }
 
 let myTree = new BST();
