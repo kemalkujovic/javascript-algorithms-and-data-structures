497ea3202698be5b4389f91f1edc5ae336fa0f2f
Kemal Kujovic <kemalkujovic1111@gmail.com>
2024-04-26 18:42:00 +0200

    DSA: DFSPreOrder Tree

--- Trees/script.js
+++ Trees/script.js
@@ -136,6 +136,17 @@ class BST {
     }
     return results;
   }
+
+  DFSPreOrder() {
+    let results = [];
+    function traverse(currentNode) {
+      results.push(currentNode.value);
+      if (currentNode.left) traverse(currentNode.left);
+      if (currentNode.right) traverse(currentNode.right);
+    }
+    traverse(this.root);
+    return results;
+  }
 }
 
 let myTree = new BST();
